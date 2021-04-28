9e0cc3636f38774965eb6759b7dcbd4657f4f026
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-04-28 10:32:32 -0700

    trying to deploy on heroku, fixed a few things on server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 8080;
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 //set up mongoose connection 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
